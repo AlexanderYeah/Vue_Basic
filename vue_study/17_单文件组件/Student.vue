3ae0758fc2a09167d620f49c1f80aeb51057cc1f
Alexander <1023954998@qq.com>
2023-04-04 22:12:02 +0800

    单文件组件

---
+++ vue_study/17_单文件组件/Student.vue
@@ -0,0 +1,32 @@
+<!-- 组件的结构 -->
+<template>
+    <div class="demo">
+        <h2>学生信息:{{name}}</h2>
+        <h2>学校地址:{{age}}</h2>
+    </div>
+</template>
+ 
+<script>    
+
+//  组件交互相关的代码
+// 默认暴露    
+export default {
+    name:'student',
+    data() {
+        return {
+            name:"Leonardo",
+            age:38
+        }
+    },
+}
+
+
+</script>
+ 
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color: rgb(196, 197, 255);
+    }
+
+</style>
