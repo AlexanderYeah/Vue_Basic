7093ec676673bd7d68783809e94d93666a63c6bd
Alexander <1023954998@qq.com>
2023-04-05 23:35:15 +0800

    配置项props

---
+++ vue_study/19_配置项props/src_props/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>          
+    <Student name="张三" sex="男" :age="88"/>    
+    <hr>
+    <Student name="李四" sex="女" :age="58"/>  
+    <hr>
+    <Student name="王老五" sex="男"/>  
+  </div>
+</template>
+
+<script>
+// 引入组件
+import Student from './components/Student.vue'
+
+export default {
+    name:'App',
+    data() {
+      return {
+        msg:'To only buy stocks currencies or cryptos on  pullbacks not breakouts'
+      }
+    },
+    components:{
+      Student 
+    },
+    methods:{
+       
+    }
+    
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue_study/19_配置项props/src_props/components/Student.vue
@@ -0,0 +1,58 @@
+<!-- 组件的结构 -->
+<template>
+    <div class="demo">
+        <h2>姓名:{{name}}</h2>
+        <h2>性别:{{sex}}</h2>
+        <h2>年纪:{{age}}</h2>
+    </div>
+</template>
+ 
+<script>    
+
+//  组件交互相关的代码
+// 默认暴露    
+export default {
+    
+    data() {
+        return {
+           
+        }
+    },
+    //方式1: 接收传递过来的数据
+    // props:['name','sex','age']
+    
+    //方式2: 接受同时限制类型
+    // props:{
+    //     name:String,
+    //     sex:String,
+    //     age:Number
+    // }
+
+    // 方式3: 可以配置默认值
+    props:{
+        name:{  
+            type:String, // 名字的类型
+            required:true // 是否是必传的
+        },
+        sex:{  
+            type:String,  
+            required:true  
+        },
+        age:{  
+            type:Number,  
+            default:99 // 不传默认99
+        }
+        
+    }
+
+}
+
+</script>
+ 
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color: cadetblue;
+    }
+
+</style>
